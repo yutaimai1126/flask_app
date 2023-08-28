fb259b8cae08ad76d8f3d8b6cad90690271bf9f9
yutaimai1126 <shitei.shintaichi@gmail.com>
2023-08-28 17:32:38 +0900

    2023.8.28

---
+++ testapp/views.py
@@ -0,0 +1,16 @@
+from flask import render_template
+from testapp import app
+
+
+@app.route('/')
+def index():
+    my_dict = {
+        'insert_something1': 'views.pyのinsert_something1部分です。',
+        'insert_something2': 'views.pyのinsert_something2部分です。',
+        'test_titles': ['title1', 'title2', 'title3']
+    }
+    return render_template('testapp/index.html', my_dict=my_dict)
+
+@app.route('/test')
+def other1():
+    return render_template('testapp/index2.html')
